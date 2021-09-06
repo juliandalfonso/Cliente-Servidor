d0ae7258aeda960665955a95f141fcd5da0bb7a9
julian <juliandavid.alfonso@utp.edu.co>
2021-09-05 21:37:30 -0500

    agregado codigo de la carpeta Banco para adaptarlo a archivos

--- BancoJson/client.py
+++ BancoJson/client.py
@@ -1,12 +1,118 @@
-import json
 import zmq
-import signal
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-
+import sys
+import json
+import time
+#Creamos un contexto de sockets
 context = zmq.Context()
+# creamos un socket como la variable s
+# REQ (REQUEST) establece el protocolo con el que interactuan los componentes
 socket = context.socket(zmq.REQ)
+# nos conectamos por medio de tcp por el puerto 5555 
+#localhost-> sistema operativo
+#quiero CONECTAR esta máquina en el puerto 5555 por medio del S.O localhost
 socket.connect('tcp://localhost:5555')
 
-socket.send_string('enviado desde el cliente')
-respuesta = socket.recv_string()
-print(respuesta)
+# crear cueenta gustavo 10mil
+# transferir gustavo 10mil julian
+# gustavo saldo
+def menu():
+    print('1.Crear cuenta\n2.Transferir saldo\n3.Mostrar saldo\n4.Depositar saldo\n5.Retirar saldo')
+    print('Seleccione una opcion: ')
+    selector = str(input())
+    return selector
+
+def JSON_crear():
+    nombre = input("\nnombre: ")
+    saldo = int(input("saldo: "))
+    crear = json.dumps(
+            {
+                "tipo" : "crear",
+                "nombre" : nombre,
+                "saldo" : saldo
+            }
+        )  
+    return crear       
+
+def JSON_mostrar():
+    nombre = input("\nnombre: ")
+    mostrar = json.dumps(
+            {
+                "tipo" : "mostrar",
+                "nombre" : nombre,
+            }
+        )
+    return mostrar
+
+def JSON_transferir():
+    remitente = input("\nremitente: ")
+    destinatario = input("\ndestinatario: ")
+    saldo = int(input("\nsaldo: "))
+    
+    mostrar = json.dumps(
+            {
+                "tipo": 'transf',
+                "remitente": remitente,
+                "destinatario" : destinatario,
+                "saldo": saldo
+            }
+        )
+    return mostrar
+
+def JSON_depositar():
+    nombre = input("\nnombre: ")
+    saldo = int(input("\nsaldo a depositar: "))
+    
+    depositar = json.dumps(
+            {
+                "tipo": 'deposito',
+                "nombre": nombre,
+                "saldo": saldo
+            }
+        )
+    return depositar
+
+def JSON_retirar():
+    nombre = input("\nnombre: ")
+    retiro = int(input("\nsaldo a retirar: "))
+    
+    retirar = json.dumps(
+            {
+                "tipo": 'retirar',
+                "nombre" : nombre,
+                "retiro": retiro
+            }
+        )
+    return retirar
+    
+
+while True:
+    
+    selector = menu()
+    
+    if selector == '1':
+        request_crear = JSON_crear()
+        socket.send_json(request_crear)
+        
+    elif selector == '2':
+        request_transferir = JSON_transferir()
+        socket.send_json(request_transferir) 
+        
+    elif selector == '3':
+        request_mostrar = JSON_mostrar()
+        socket.send_json(request_mostrar)        
+    
+    elif selector == '4':
+        request_depositar = JSON_depositar()
+        socket.send_json(request_depositar)        
+    
+    elif selector == '5':
+        request_retirar = JSON_retirar()
+        socket.send_json(request_retirar)        
+    else:
+        print('seleccion no valida')
+    
+    
+    response = socket.recv_string()
+    print(response)
+    time.sleep(3)
+    
--- BancoJson/server.py
+++ BancoJson/server.py
@@ -9,12 +9,99 @@ socket = context.socket(zmq.REP)
 socket.bind('tcp://*:5555')
 
 while True:
-    mensaje = socket.recv_string()
-    print(mensaje)
+    #Manejo de archivos 
     file = open("./DATABASE.json", "r")
     data=file.read()
-    data_json = json.loads(data)
-    print(data_json)
+    DATABASE = json.loads(data)
     file.close()
-    socket.send_string('listo')
+    
+    request = socket.recv_json()
+    #pasamos a diccionario
+    request_dict = json.loads(request)
+
+    if request_dict["tipo"] == 'crear':
+        #creamos un nuevo objeto en python
+        nombre = request_dict["nombre"]
+        saldo = request_dict["saldo"]  
+        #verificamos la existencioa del nuevo contacto
+        if nombre in DATABASE:
+            socket.send_string('\nUsuario Ya Existente')
+        else:
+            nuevo_dato = {nombre:saldo}
+            file = open("./DATABASE.json", "w")
+            append = json.dumps(nuevo_dato)
+            file.write(append)
+            file.close()
+            response = f'\nagregado->{nombre} con saldo {saldo}\n'
+            socket.send_string(response)
+        
+    elif request_dict["tipo"] == 'transf':
+        
+        remitente = request_dict["remitente"]
+        destinatario = request_dict["destinatario"]
+        saldo = request_dict["saldo"]
+        
+        #verificamos la existencia de los usuarios
+        if remitente in DATABASE and destinatario in DATABASE:
+                #verificamos la disponibilidad del remitente
+            check_saldo = DATABASE[remitente] - saldo
+            if check_saldo < 0:
+                socket.send_string('\nSaldo insuficiente')
+            else:
+                #creamos una nueva lista y la actualizamos al DATABASE
+                nuevo_saldo_remitente = DATABASE[remitente]-saldo
+                nuevo_remitente = {remitente : nuevo_saldo_remitente}
+                DATABASE.update(nuevo_remitente)
+                
+                nuevo_saldo_destinatario = DATABASE[destinatario]+saldo
+                nuevo_destinatario = {destinatario : nuevo_saldo_destinatario}
+                DATABASE.update(nuevo_destinatario)
+                response= f'\nTransferencia de {remitente} a {destinatario} por {saldo}\n'
+                socket.send_string(response)
+        #caso en que no exista algun usuario
+        else:
+            socket.send_string('\nusuario no encontrado')
+        
+        
+        
+    elif request_dict["tipo"] == 'mostrar':
+        
+        nombre = request_dict["nombre"]
+        #verificamos la existencia de los usuarios
+        if nombre in DATABASE:
+            response= f'\nEl saldo de {nombre} es: {DATABASE[nombre]}\n'
+            socket.send_string(response)
+        else:
+            socket.send_string('\nusuario no encontrado')
+    
+    elif request_dict["tipo"] == 'deposito':
+        if nombre in DATABASE:
+            nombre = request_dict["nombre"]
+            nuevo_saldo = DATABASE[nombre]+request_dict["saldo"]
+            depositado = {nombre:nuevo_saldo}
+            DATABASE.update(depositado)
+            response= f'\nNuevo saldo de {nombre} es {DATABASE[nombre]}\n'
+            socket.send_string(response)
+        else:
+            socket.send_string('\nusuario no encontrado')
+    
+    elif request_dict["tipo"] == 'retirar':
+        nombre = request_dict["nombre"]
+        #verificamos la existencia de los usuarios
+        if nombre in DATABASE:
+            
+            nuevo_saldo = DATABASE[nombre]-request_dict["retiro"]
+            if nuevo_saldo < 0:
+                socket.send_string('\nSaldo insuficiente')
+            else:
+                retirado = {nombre:nuevo_saldo}
+                DATABASE.update(retirado)
+                response= f'\nNuevo saldo de {nombre} es {DATABASE[nombre]}\n'
+                socket.send_string(response)
+        else:
+            socket.send_string('\nusuario no encontrado')
+    else:
+        print('error')
+    
+    print(DATABASE)
     
