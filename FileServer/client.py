0fed185903f3d0ce4397bc880c76e122c3c951ed
julian <juliandavid.alfonso@utp.edu.co>
2021-09-08 23:19:26 -0500

    cliente funcionalidad 1.upload completada, envia json+'\n'+archivo

--- FileServer/client.py
+++ FileServer/client.py
@@ -1,37 +1,32 @@
 
 #todo:-----------------------------------------
-#python client.py username upload hola.txt
-#python client.py username sharelink hola.txt
-#python client.py username downloadlink link
-#python client.py username list hola.txt
+    #python client.py username upload hola.txt
+        #crear funcionalidad upload ----------LISTO
+    #python client.py username sharelink hola.txt
+        #crear funcionalidad sharelink
+    #python client.py username downloadlink link
+    #python client.py username list hola.txt
 #todo:-----------------------------------------
 
 import zmq
 import json
 import sys
 
+#-----------Conexion con SERVER-------------
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect('tcp://localhost:5555')
+#-----------Conexion con SERVER-------------
 
-#------------Recive Argumentos --------------
+
+
+#------------Funciones--------------
 def arguments():
     usuario = sys.argv[1]
     tipo = sys.argv[2]
     dir_archivo = sys.argv[3]
     
     return usuario, tipo, dir_archivo
-
-
-
-
-#----------MANEJO DE ARCHIVOS-------------
-file = open("./files/hola.txt", "rb")
-data=file.read()
-file.close()
-print(type(data))
-#----------MANEJO DE ARCHIVOS-------------
-
 
 def convertToJson(user, tipo):
     crearJson = json.dumps(
@@ -42,20 +37,40 @@ def convertToJson(user, tipo):
     )
     return crearJson
 
-def procesaArchivo():
-    pass
+def procesaArchivo(file_dir):
+    file = open(file_dir, "rb")
+    data=file.read()
+    file.close()
+    return data
 
-while True:
-    
-    user, tipo, file_dir = arguments()
-    
-    #devuelve el json
-    newjson = convertToJson(user, tipo)
-    print(type(newjson))
-    #procesa la direccion 
-    archivo = procesaArchivo(file_dir)
-    
-    print(user, tipo, dir)
-    socket.send(data)
-    response = socket.recv_string()
-    print(response)
+def uneJsonyArchivo(byte_json, byte_archivo):
+    unido = byte_json + byte_archivo
+    return unido
+
+
+
+
+
+#!-------------------Logica del CLIENT -------------------------
+#recibimos los argumentos del usuario y los guardamos en variables
+user, tipo, file_dir = arguments()
+
+#creamos un json con la informacion que suministra el usuario
+newjson = convertToJson(user, tipo)
+#convierto el json a binario y le agregamos un salto de linea
+byte_json = bytes(newjson, 'utf-8')
+byte_jump = bytes('\n', 'utf-8')
+jsonbinario = byte_json + byte_jump
+
+#procesa la direccion del archivo lo lee y devuelve su informacion en binario
+archivobinario = procesaArchivo(file_dir)
+
+#unimos el jsonbinario(informacion de cliente) y el archivo en binario
+request = uneJsonyArchivo(jsonbinario, archivobinario)
+
+#enviamos la peticion al server
+socket.send(request)
+#esperamos la respuesta y la imprimimos
+response = socket.recv_string()
+print(response)
+#!-------------------Logica del cliente -------------------------
--- FileServer/server.py
+++ FileServer/server.py
@@ -1,16 +1,53 @@
-import zmq
-import os
 
+#todo:--------------------------------------------------
+    #UPLOAD
+    #separar archivo json de binario
+        #convertir binario a string
+        #guardar json en una variable
+        #guardar archivo recibido
+
+    #SHARELINK
+    #crear DATABASE json
+        #guardar usuario en DATABASE
+    #crear sistema de ID's personalizado para los links
+        # {
+        #     "juan":
+        #         {
+        #             "123-abc" : "./files/hola.txt",
+        #             "456-def" : "./files/song.mp3"
+        #         }
+        # }
+
+    #DOWNLOADLINK
+        #verificar existencia de link
+        #devolver archivo binario
+#todo:--------------------------------------------------
+
+
+
+import zmq
+import json
+
+#-----------Conexion con CLIENT-------------
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind('tcp://*:5555')
+#-----------Conexion con CLIENT-------------
 
 
-mens = socket.recv()
-file = open('./files/hola_nuevo.txt', 'wb' )
-file.write(mens)
-# save(DATABASE, file)
-
-file.close()
-print('guardado')
-socket.send_string('guardado en DB')
+#!-------------------Logica del SERVER -------------------------
+while True:
+    #Recibimos un archivo binario
+    mens = socket.recv()
+    #creamos un nuevo archivo en binario
+    file = open('./files/hola_nuevo.txt', 'wb')
+    #le escribimos lo que contiene el mensaje
+    file.write(mens)
+    #imprimimos el mensaje recibido
+    print(mens)
+    # save(DATABASE, file)
+    #cerramos el archivo
+    file.close()
+    print('guardado')
+    socket.send_string('guardado en DB')
+#!-------------------Logica del SERVER -------------------------
