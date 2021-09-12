79c3539ef3cab6f329106d067f607be218d7c9ef
julian <juliandavid.alfonso@utp.edu.co>
2021-09-11 19:43:33 -0500

    server guarda json en DATABASE con link y archivo en carpeta files

--- FileServer/server.py
+++ FileServer/server.py
@@ -35,47 +35,71 @@ socket = context.socket(zmq.REP)
 socket.bind('tcp://*:5555')
 #-----------Conexion con CLIENT-------------
 
+
+def actualizaDB():
+    file = open("./DATABASE.json", "w")
+    appendjson = json.dumps(DATABASE, indent=4)
+    file.write(appendjson)
+    file.close()
+
+def nuevoDict(json_dic, link):
+    filename = json_dic["filename"]
+    newjson = {filename : link}
+    return newjson
+
 def nuevoUsuario(json_dic, link):
-    
-    # newjson = json.dumps(
-    #         {
-    #             link : json_dic["filename"]
-    #         }
-    #     )
-    # return newjson
-    pass
-    
+    nombre = json_dic["usuario"]
+    filename = json_dic["filename"]
+    newuser =   { nombre : {filename : link}}
+    return newuser
+
+def guardaArchivo(archivo, json_dict):
+    newfilepath = './files/'+json_dic["filename"]
+    file = open(newfilepath, "wb")
+    file.write(archivo)
+    file.close()
 
 def procesaJson(mensjson):
     jsondecoded = mensjson.decode('utf-8')
     finaljson = json.loads(jsondecoded)
     return finaljson
 
-def crear():
+def upload():
     #creamos un nuevo objeto en python
     nombre = json_dic["usuario"]
     file_dir = json_dic["filename"]  
-    link = uuid.uuid4()
+    #creaamos un nuevo id (link)
+    link = str(uuid.uuid4())
     
-    #!link = linkscreator()
     #verificamos la existencioa del nuevo contacto
     if nombre in DATABASE:
-        socket.send_string('\nUsuario Ya Existente')
-        #!agregarLink(link)
-        #!guardaArchivo(link)
-        
-    else:
-        #!nuevoUsuario()
-        jsonlink = nuevoUsuario(json_dic, link)
-        #!guardaArchivo(link)
-        DATABASE[nombre] = file_dir
-        file = open("./DATABASE.json", "w")
-        appendjson = json.dumps(DATABASE)
-        file.write(appendjson)
-        file.close()
-        
-        response = f'\nagregado->{file_dir} de {nombre}\n'
+        #creamos el diccionario a agregar al usuario existente en la BD
+        newjson = nuevoDict(json_dic, link)
+        #agregamos el nuevo diccionario al usuario
+        DATABASE[nombre].update(newjson)
+        #abrimos el archivo de base de datos y lo agregamos
+        actualizaDB()
+        #enviamos la respuesta al cliente
+        response = f'\nSe agregó el archivo:{file_dir} al usuario {nombre}'
         socket.send_string(response)
+        #! agregar archivo a carpeta de usuario
+    
+    #caso en que el usuario no esté en la BD
+    else:
+        #creamos el diccionario del nuevo usuario
+        newuser = nuevoUsuario(json_dic, link)
+        #actualizamos el nuevo usuario en la BD
+        DATABASE.update(newuser)
+        actualizaDB()
+        #respondemos al cliente
+        response = f'\nnuevo usuario {nombre} con archivo {file_dir}'
+        socket.send_string(response)
+        
+        #!crear carpeta de usuario
+        #!agregar archivo a carpeta de  usuario
+
+
+
 
 #!-------------------Logica del SERVER -------------------------
 while True:
@@ -89,9 +113,11 @@ while True:
     file.close()
 
     json_dic = procesaJson(mens[0])
-    
+    archivo = mens[1]
+    guardaArchivo(archivo, json_dic)
+    #caso en que el usuario haga upload
     if json_dic["tipo"] == 'upload':
-        crear()
+        upload()
     
     print('guardado')
 #!-------------------Logica del SERVER -------------------------
