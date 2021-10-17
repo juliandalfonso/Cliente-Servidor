64f91dfc0dc90e9bd3f6e61d52ddeb378baee1dc
julian <juliandavid.alfonso@utp.edu.co>
2021-10-17 17:39:27 -0500

    feat: agregado menu al cliente se agrego un menu basico al cliente con las opciones de subir y bajar archivos

--- 12.FileServerTorrent/client.py
+++ 12.FileServerTorrent/client.py
@@ -6,21 +6,47 @@
 
 import sys
 import hashlib
+import zmq
+import os
 
 CHUNK_SIZE = 10
-sha1 = hashlib.sha1()
-filename = sys.argv[1]
-file = open(filename, "rb")
+
+def hashear():    
+    sha1 = hashlib.sha1()
+    filename = sys.argv[1]
+    file = open(filename, "rb")
+    while True:
+        data = file.read(CHUNK_SIZE)
+        if not data:
+            break
+        sha1.update(data)
+        chunkHash= sha1.hexdigest()    
+        print(data)
+        print(chunkHash)
+        print(int(chunkHash,16))
+    
+    
+    print(f"SHA1: {sha1.hexdigest()}")
+
+
+#logica del menú para mejor experiencia de usuario
+def menuDatos():
+    #borramos la pantalla para mejor interfaz
+    os.system('cls||clear')
+    print('1.upload\n2.Download\n')
+    print('Seleccione una opcion: ')
+    selector = str(input())
+    op=''
+    if selector == '1':
+        os.system('cls||clear')
+        print('Ingresa el archivo subida:\n') 
+        op = str(input())
+    elif selector == '2':
+        os.system('cls||clear')
+        print('Ingresa el archivo descarga:\n') 
+        op = str(input())
+    
+    return op
 
 while True:
-    data = file.read(CHUNK_SIZE)
-    if not data:
-        break
-    sha1.update(data)
-    chunkHash= sha1.hexdigest()    
-    print(data)
-    print(chunkHash)
-    print(int(chunkHash,16))
-    
-    
-print(f"SHA1: {sha1.hexdigest()}")
+    op = menuDatos()
