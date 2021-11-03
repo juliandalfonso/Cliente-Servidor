e9c3446ef02da3274124b65401933304d7447525
julian <juliandavid.alfonso@utp.edu.co>
2021-11-02 22:25:01 -0500

    feat:Agrega nuevo server a DB al inicializar

--- 12.FileServerTorrent/server/server.py
+++ 12.FileServerTorrent/server/server.py
@@ -1,8 +1,17 @@
 
 #todo: ----------------------------------------------------
 #crea logica de rangos para cada servidor
+
+#! idea de implementacion json por servidor
+# {
+#     "server1":{
+#     "add":"192.16.31.32",
+#     "port":"4000",
+#     "range":[0,105312291668557186697918027683670432318895095400549111254310977536]}
+# }
 #todo: ----------------------------------------------------
 
+import json
 import os
 import socket
 import uuid
@@ -17,38 +26,73 @@ context = zmq.Context()
 client_socket = context.socket(zmq.REP)
 client_socket.bind('tcp://*:1111')
 #!-----------Conexion con Client-------------
-            
-#MAC address
-MAC=str(hex(uuid.getnode()))
-#IP address
-IP=socket.gethostbyname(socket.gethostname())
-#Process ID
-PID=str(os.getpid())
-#string de 100 caracteres aleatorios
-RAND=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(100))
-#String concatenado
-S=IP+MAC+PID+RAND
-#objeto que permite encriptar un archivo a 160bits - 20 bytes
-sha1 = hashlib.sha1()
-sha1.update(S.encode('utf-8')) 
-Shashed= sha1.hexdigest()
-#convertimos a decimal el String Hasheado 
-dec=int(Shashed, 16)
-print(f'String = IP + MAC + PID + RAND \n')
-print(f'String = {S} \n')
-print(f'Hash = {Shashed} \n')
-print(f'decimal = {dec} \n')
 
-#esperamos que el usuario digite enter para volver al menu
-str(input('\n\npresione enter para continuar'))
+DECIMALID = 0
 
-def inicializar(port):
+
+def idGenerator():
+    #MAC address
+    MAC=str(hex(uuid.getnode()))
+    #IP address
+    IP=socket.gethostbyname(socket.gethostname())
+    #Process ID
+    PID=str(os.getpid())
+    #string de 100 caracteres aleatorios
+    RAND=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(100))
+    #String concatenado
+    S=IP+MAC+PID+RAND
+    #objeto que permite encriptar un archivo a 160bits - 20 bytes
+    sha1 = hashlib.sha1()
+    sha1.update(S.encode('utf-8')) 
+    Shashed= sha1.hexdigest()
+    #convertimos a decimal el String Hasheado 
+    dec=int(Shashed, 16)
+    print(f'String = IP + MAC + PID + RAND \n')
+    print(f'String = {S} \n')
+    print(f'Hash = {Shashed} \n')
+    print(f'decimal = {dec} \n')
+    DECIMALID = dec
+    #esperamos que el usuario digite enter para volver al menu
+    str(input('\n\npresione enter para continuar'))
+    
+
+
+#lee el contenido de DB y retorna su contenido
+def leeDB(file):
+    file = open(file, "r")
+    data=file.read()
+    DATABASE = json.loads(data)
+    file.close()
+    return DATABASE
+
+#sobreescribe el nuevo contenido en la BD ingresada
+def actualizaDB(file,DB):
+    file = open(file, "w")
+    appendjson = json.dumps(DB, indent=4)
+    file.write(appendjson)
+    file.close()
+
+
+#funcion que inicializa un servidor por primera vez
+def inicializar(server,ip, port):
+    #crear json
+    newserver =   {
+        "add":ip,
+        "port":port,
+        "range":[0,105312291668557186697918027683670432318895095400549111254310977536]
+        }
+    serverdb.update(newserver)
+    actualizaDB('./serverdb.json',serverdb)
+    #subir a serverdb
+    
+    ipport = ip+':'+port
     server = '1'
     range = '[0,2^160]'
-    return port, server, range
+    return ipport, server, range
 
 #funcion que valida la existencia de una ip y un puerto
-def validarExistenciaIP(ipport):
+def validarExistenciaIP(ip,port):
+    
     pass
     
 
@@ -64,14 +108,15 @@ def menuDatos():
     
     if selector == '1':
         os.system('cls||clear')
-        ipport = str(input('\n ingrese ip:puerto: '))
-        existe = validarExistenciaIP(ipport)
+        ip= str(input('\n ingrese ip: '))
+        port= str(input('\n ingrese port: '))
+        existe = validarExistenciaIP(ip,port)
 
         if existe:
             print('esta direccion ya esta ocupada')
         else:
-            
-            ipport,server,range = inicializar(ipport)            
+            #inicializamos el primer servidor
+            ipport,server,range = inicializar('server1',ip,port)            
             print(f'\nAddress: {ipport}\n')
             print(f'Servidor No: {server}\n')
             print(f'Rango asignado {range}\n')
@@ -93,7 +138,12 @@ def menuDatos():
         print('digite correctamente el comando')
         
 
+#antes de empezar el ciclo encontramos el rango 
+idGenerator()
+
 while True:
+    #cargamos la base de datos en DATABASE
+    serverdb = leeDB('./serverdb.json')
     menuDatos()
 
     #esperamos que el usuario digite enter para volver al menu
